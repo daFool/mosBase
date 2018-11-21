047f0becce57ff7c8b9fc1ba7c64cb9e961d8971
Mauri Sahlberg <mos@hoopo.kotikolo>
2018-11-21 17:55:08 +0200

    Lisätty iecLuvut.

---
+++ sql/functions/iecLuvut.sql
@@ -0,0 +1,82 @@
+drop function if exists isoLuku(bigint);
+drop function if exists isoLukuLiite(bigint);
+drop function if exists stringiLuku(bigint);
+drop function if exists isoSkaala(bigint, char(1));
+
+create function iecSkaala(luku bigint, liite char(2)) returns numeric as '
+    declare i int;
+    declare k numeric;
+    declare liitteet varchar[];
+ 
+    begin
+       liitteet := ARRAY[''B'', ''kiB'', ''MiB'', ''GiB'', ''TiB'', ''PiB'', ''YiB''];
+
+        i := 1;
+        loop
+            if liitteet[i]=liite then
+                exit;
+            end if;
+            i := i+1;
+            if i > 7 then
+                exit;
+            end if;
+        end loop;
+        i := i-1;
+        k := round((luku::numeric*100 / pow(1024,i))) / 100;
+        return k;
+    end;        
+'
+LANGUAGE plpgsql;
+
+create function iecLuku(luku bigint) returns numeric as '
+    declare i int;
+    declare k numeric;
+        
+    begin
+        if luku is null or luku=0 then
+            i:=0;
+        else
+            i := floor(log(1000,luku));
+        end if;
+        k := round((luku::numeric*100 / pow(1024,i))) / 100;
+        return k;
+    end;
+
+'
+LANGUAGE plpgsql;
+
+create function iecLukuLiite(luku bigint) returns varchar as '
+    declare i int;
+    declare k numeric;
+    declare tulos varchar(255);
+    declare l varchar(2);
+    declare alku varchar(255);
+    declare liitteet varchar[];
+
+    begin
+        liitteet := ARRAY[''B'', ''kiB'', ''MiB'', ''GiB'', ''TiB'', ''PiB'', ''YiB''];
+        if luku is null or luku=0 then
+            i:=0;
+        else
+            i := floor(log(1024,luku));
+        end if;
+        l := liitteet[i+1];
+        return l;
+    end;
+' LANGUAGE plpgsql;
+
+create function stringiLevyLuku(luku bigint) returns varchar as '
+    declare i int;
+    declare k numeric;
+    declare l varchar(2);
+    declare alku varchar(255);
+    
+    begin
+        select into k iecLuku(luku);
+        select into l iecLukuLiite(luku);
+        
+        alku := to_char(k,''999.99'');
+        return concat(alku, l);
+    end;
+'
+LANGUAGE plpgsql;
